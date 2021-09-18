b38e519f8db4d5fbf0b8d8fd668468ffe028c138
Shueb Dalvi <shueb123@gmail.com>
2021-09-18 21:40:04 +0530

    initial commit

---
+++ src/test/java/com/shuebdalvi/registry/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuebdalvi.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
